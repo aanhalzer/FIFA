17b20574a0c4bb7b0c491e277b20b0d89327205d
Andres <andresanhalzer@hotmail.com>
2018-09-19 22:21:39 -0400

    Starting ViewModels

---
+++ FIFA/ViewModels/WeekInputViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIFA.ViewModels
+{
+    class WeekInputViewModel : MainViewModelBase
+    {
+        private string semana_ = "00-0000";
+        public string Semana {
+            get { return semana_; }
+            set { semana_ = value; OnPropertyChanged("Semana"); }
+        }
+
+        private int mortalidad_ = 6;
+        public int Mortalidad {
+            get { return mortalidad_; }
+            set { mortalidad_ = value; OnPropertyChanged("Mortalidad"); }
+        }
+
+        private int liquidacion_ = 0;
+        public int Liquidacion {
+            get { return liquidacion_; }
+            set { liquidacion_ = value; OnPropertyChanged("Liquidacion"); }
+        }
+
+    }
+}
