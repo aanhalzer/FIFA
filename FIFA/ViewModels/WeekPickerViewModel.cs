a2812ddcd725fcd1226271a5f52c50419ae572a9
Andres <andresanhalzer@hotmail.com>
2019-04-09 22:02:33 -0400

    Some bindings on WeekPicker

--- FIFA/ViewModels/WeekPickerViewModel.cs
+++ FIFA/ViewModels/WeekPickerViewModel.cs
@@ -7,12 +7,26 @@ using System.Windows.Input;
 
 namespace FIFA.ViewModels
 {
-    class WeekPickerViewModel : MainViewModelBase
+    public class WeekPickerViewModel : MainViewModelBase
     {
         public void PageLoad() => OnLoad();
         private void OnLoad() 
         {
 
+        }
+
+        private string nuevaSemana_ = string.Empty;
+
+        private string semanaCrearText_ = string.Empty;
+        public string SemanaCrearText {
+            get { return semanaCrearText_; }
+            set { semanaCrearText_ = value; OnPropertyChanged("SemanaCrearText"); }
+        }
+
+        private string editarSemana_ = "00-0000";
+        public string EditarSemana {
+            get { return editarSemana_; }
+            set { editarSemana_ = value; OnPropertyChanged("EditarSemana"); }
         }
     }
 }
